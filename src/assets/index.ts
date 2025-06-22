0de5e606ab915920cdcbcd527c30d69d21c9dc52
Kjiw0n <rwd0904@naver.com>
2025-06-22 16:27:04 +0900

    style: setting assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,6 +5,7 @@ import IcnCheckBoxUnCheck from "@/assets/icon_checkbox_uncheck.svg";
 import IcnMinus from "@/assets/icon_minus.svg";
 import IcnPlus from "@/assets/icon_plus.svg";
 import SampleImg from "@/assets/sampleImg.svg";
+import MaskGroup from "@/assets/MaskGroup.svg";
 
 const Icn = {
   IcnShare,
@@ -14,6 +15,7 @@ const Icn = {
   IcnMinus,
   IcnPlus,
   SampleImg,
+  MaskGroup,
 };
 
 export default Icn;
